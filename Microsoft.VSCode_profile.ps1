44ec62f462be01396544ca860c1f2a3cea24de48
Chris <69024420+TheJenious0917@users.noreply.github.com>
2021-03-04 23:34:51 -0600

    Add files via upload

---
+++ Microsoft.VSCode_profile.ps1
@@ -0,0 +1,23 @@
+function prompt {
+    $Date = Get-Date -Format 'hh:mm:ss tt'
+
+    # Test for Admin / Elevated
+    $IsAdmin = (New-Object Security.Principal.WindowsPrincipal ([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+
+    $GitPromptSettings.EnableWindowTitle = $null
+
+    #Decorate the CMD Prompt
+    Write-Host ""
+    Write-Host ($(if ($IsAdmin) { 'Elevated ' } else { '' })) -BackgroundColor DarkRed -ForegroundColor White -NoNewline
+    Write-Host " $env:USERNAME@$env:COMPUTERNAME " -BackgroundColor DarkBlue -ForegroundColor White -NoNewline
+    Write-Host " $pwd "  -ForegroundColor White -BackgroundColor DarkGreen -NoNewline
+    
+    Write-Host " [$date] " -ForegroundColor White
+
+    return "$(Write-VcsStatus) => "
+}
+
+# Set-location C:\WorkRepos
+#Write-Host 'importing posh-git' -ForegroundColor Yellow
+Import-Module posh-git
+Write-Host
